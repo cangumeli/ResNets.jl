1d789203c80a908888eabdffb41cab4cdcaf6931
cangumeli <cgumeli@ku.edu.tr>
2017-04-01 02:48:25 +0300

    Train/Val split

--- softmax.jl
+++ softmax.jl
@@ -5,21 +5,18 @@ end
 include("data.jl")
 include("imgproc.jl")
 using Knet
-using ArgParse # To work with command line argumands
-using Compat,GZip # Helpers to read the MNIST (Like lab-2)
-using data
 
 
 function main(args="")
 
-    s = ArgParseSettings()
+    #=s = ArgParseSettings()
     @add_arg_table s begin
         ("--epochs"; arg_type=Int; default=10; help="number of epoch ")
         ("--batchsize"; arg_type=Int; default=100; help="size of minibatches")
         ("--hidden"; nargs='*'; arg_type=Int; help="sizes of hidden layers, e.g. --hidden 128 64 for a net with two hidden layers")
         ("--lr"; arg_type=Float64; default=0.5; help="learning rate")
         ("--winit"; arg_type=Float64; default=0.1; help="w initialized with winit*randn()")
-    end
+    end=#
 
         #=
     the actual argument parsing is performed via the parse_args function the result
@@ -27,26 +24,42 @@ function main(args="")
     "batchsize", "hidden" and "lr", "winit" so that e.g. o["lr"] or o[:lr]
      will yield the value associated with the positional argument.
      For more information: http://argparsejl.readthedocs.io/en/latest/argparse.html
-    =#
-    o = parse_args(s; as_symbols=true)
+
+    o = parse_args(s; as_symbols=true)=#
 
     # Some global configs do not change here
-    println("opts=",[(k,v) for (k,v) in o]...)
-    ((xtrn, ytrn), (xtst, ytst)) = data.cifar10()
-    mean_trn = imgproc.mean_subtract!(xtrn; mode=:pixel)
-    xtst .-= mean_trn
-    xtrn = mat(xtrn)
-    xtst = mat(xtst)
-    # println("Mean trn", size(mean_trn))
+    #println("opts=",[(k,v) for (k,v) in o]...)
 
+    # println("Mean trn", size(mean_trn))
+    dtrn, dval, dtst = loaddata()
     # println(size(xtrn))
     #dtrn = minibatch(xtrn, ytrn, o[:batchsize])
     #dtst = minibatch(xtst, ytst, o[:batchsize])
-    w = init_params(size(xtrn, 1), size(ytrn,1))
+    # println(typeof(dtrn[1]))
+    w = init_params(size(dtrn[1], 1), size(dtrn[2],1))
     # helper function to see how your training goes on.
     # report(epoch)=println((:epoch,epoch,:trn,accuracy(w,dtrn),:tst,accuracy(w,dtst)))
-    train(w, (xtrn, ytrn), (xtst, ytst))
+    train(w, dtrn, dval; num_iters=1000)
+    println("Final accuracy ", accuracy(w, dtst)[1])
     return w
+end
+
+function loaddata(nval=5000)
+   ((xtrn, ytrn), (xtst, ytst)) = data.cifar10()
+   ntrain = size(xtrn, 4)
+   order = shuffle(1:ntrain)
+   (xval, yval) = (xtrn[:, :, :, 1:nval], ytrn[:, 1:nval])
+   (xtrn, ytrn) = (xtrn[:, :, :, (nval+1):ntrain], ytrn[:, (nval+1):ntrain])
+   mean_trn = imgproc.mean_subtract!(xtrn; mode=:pixel)
+   println(size(xtst))
+   println(size(mean_trn))
+   xtst .-= mean_trn
+   xval .-= mean_trn
+   println(size(xtrn))
+   xtrn = mat(xtrn)
+   xval = mat(xval)
+   xtst = mat(xtst)
+   return (xtrn, ytrn), (xval, yval), (xtst, ytst)
 end
 
 function init_params(ninputs, noutputs, winit=0.0001)
@@ -77,7 +90,7 @@ end
 
 lossgradient =  grad(loss)# your code here [just 1 line]
 
-function train(w, dtrn,dtst; lr=0.01, num_iters=10000, print_period=50)
+function train(w, dtrn, dval; lr=0.01, num_iters=10000, print_period=50)
    println("Iter/Training accuracy: ",0, "/", accuracy(w, dtrn)[1])
    println("Iter/Test accuracy: ",0, "/", accuracy(w, dtst)[1])
    println("")
@@ -96,14 +109,14 @@ function train(w, dtrn,dtst; lr=0.01, num_iters=10000, print_period=50)
     return w
 end
 
-function accuracy(w,dtst,pred=predict)
+function accuracy(w,dtst,pred=predict,bsize=200)
     ncorrect = 0
     ninstance = 0
     nloss = 0
     (X, Y) = dtst
-    for i = 1:200:size(X,2)
-      x = X[:, i:i+199]
-      ygold = Y[:, i:i+199]
+    for i = 1:bsize:size(X,2)
+      x = X[:, i:i+bsize-1]
+      ygold = Y[:, i:i+bsize-1]
       ypred = predict(w, x)
       nloss += loss(w, x, ygold)
       ncorrect += sum(ygold .* (ypred .== maximum(ypred,1)))
